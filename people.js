b0f4dda1755b91e31979b4f7c0748f5a1fddd038
sinigers <sinigers@yahoo.com>
2021-06-14 09:26:36 +0300

    creating server

--- people.js
+++
@@ -1,8 +0,0 @@
-const people=['yoshi', 'rui', 'maria', 'mario'];
-// console.log(people);
-const ages = [20, 30, 35, 25];
-// module.exports = people;
-
-module.exports = {
-    people, ages
-};
--- test.js
+++
@@ -1,9 +0,0 @@
-// const name = 'yoshi';
-
-// console.log(name);
-const greet = (name) => {
-    console.log(`hello, ${name}`);
-}
-
-greet('maria');
-greet('yoshi');
