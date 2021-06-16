337ab4f525436e8524fa9412e323f6a83c3764dd
sinigers <sinigers@yahoo.com>
2021-06-16 13:02:24 +0300

    using next(), static files

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 // to start app 'nodemon app'in terminal
 
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
 
 // listen for requests
 app.listen(3000);
+
+//midleware static files - files which browser can see/render/access everyting in public folder can be seen from frontEnd
+app.use(express.static('public'));
+
+
+// //middleware morgan - logger
+// app.use(morgan('dev'));
+
+// //create middleware
+// app.use((req, res, next) =>{
+//   console.log(('new request made:'));
+//   console.log(('host:', req.hostname));
+//   console.log(('path:', req.path));
+//   console.log(('method:', req.method));
+
+//   //next() after req/res continue to next code
+//   next();
+// });
+
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -20,6 +40,10 @@ app.get('/', (req, res) => {
   ];
   res.render('index', { title: 'Home', blogs });
 });
+// app.use((req, res, next) =>{
+//   console.log('in the next middleware');
+//   next();
+// });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
